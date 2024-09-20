5ea98a119bc9a16355010f3d7a09328c18752a4a
yiwangio <113520262+yiwangio@users.noreply.github.com>
2024-09-20 14:47:29 +0300

    string errors related to engine

---
+++ cmd/syntax/errors.go
@@ -0,0 +1,8 @@
+package syntax
+
+var (
+	invalidArgsNumErr  = "Invalid args number"
+	keyNotExistsErr    = "No key found"
+	commandNotFoundErr = "No command found"
+	emptyLineErr       = "Empty line provided"
+)
